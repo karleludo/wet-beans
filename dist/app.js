d6f5371a79b785aab44b4eb07a50a688931a253a
karl_eludo <k_eludo@icloud.com>
2022-02-02 00:27:53 +0800

    implement a really(reallllyyy) simple coffee ratio calculator that goes only one way...

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,17 @@
 "use strict";
+let ratioWater;
+let ratioCoffee;
+let resultCoffee = 15;
+let resultWater;
+// document.getElementById("result-water");
+// document.getElementById("result-coffee");
+function setRatio() {
+    ratioWater = +document.getElementById("ratio-water").value;
+    ratioCoffee = +document.getElementById("ratio-coffee").value;
+}
+function getResult() {
+    setRatio();
+    resultCoffee = +document.getElementById("result-coffee").value;
+    resultWater = resultCoffee * (ratioWater / ratioCoffee);
+    document.getElementById("result-water").value = (Math.floor(resultWater)).toString();
+}
