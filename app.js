9a459fc8bdf3139e7b9f64e36e9580c53ed76463
karl_eludo <k_eludo@icloud.com>
2022-02-04 16:38:36 +0800

    I'm removing all typescript files because you can't run before you can walk... relearning javascript lol

---
+++ app.js
@@ -0,0 +1,43 @@
+"use strict";
+const ratioCoffee = document.getElementById("ratio-coffee");
+let objectRatioCoffee = {};
+const ratioWater = document.getElementById("ratio-water");
+let objectRatioWater = {};
+const recipeCoffee = document.getElementById("recipe-coffee");
+let objectRecipeCoffee = {};
+const recipeWater = document.getElementById("recipe-water");
+let objectRecipeWater = {};
+Object.defineProperty(objectRatioCoffee, "value", {
+    get() {
+        return parseInt(ratioCoffee.value);
+    },
+    set(newValue) {
+        ratioCoffee.value = newValue;
+    }
+});
+Object.defineProperty(objectRatioWater, "value", {
+    get() {
+        return parseInt(ratioWater.value);
+    },
+    set(newValue) {
+        ratioWater.value = newValue;
+    }
+});
+Object.defineProperty(objectRecipeCoffee, "value", {
+    get() {
+        return parseInt(recipeCoffee.value);
+    },
+    set(newValue) {
+        recipeCoffee.value = newValue;
+    }
+});
+Object.defineProperty(objectRecipeWater, "value", {
+    get() {
+        return parseInt(recipeWater.value);
+    },
+    set(newValue) {
+        recipeWater.value = newValue;
+    }
+});
+function CalculateRecipeCoffee() {
+}
