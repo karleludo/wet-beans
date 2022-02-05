6e7a2c4263595c1fe6bb4c21b82080d1eb6d79e5
karl_eludo <k_eludo@icloud.com>
2022-02-05 22:15:12 +0800

    added footer, credits, footer styles, code prettier

--- app.js
+++ app.js
@@ -5,43 +5,43 @@ const recipeCoffee = document.getElementById("recipe-coffee");
 const recipeWater = document.getElementById("recipe-water");
 
 //instantiate some objects
-let objectRatioCoffee = {}
+let objectRatioCoffee = {};
 let objectRatioWater = {};
 let objectRecipeCoffee = {};
 let objectRecipeWater = {};
 
 //bind input to object for data-binding
 Object.defineProperty(objectRatioCoffee, "value", {
-    get() {
-        return parseInt(ratioCoffee.value);
-    },
-    set(newValue) {
-        ratioCoffee.value = newValue;
-    }
+  get() {
+    return parseInt(ratioCoffee.value);
+  },
+  set(newValue) {
+    ratioCoffee.value = newValue;
+  },
 });
 Object.defineProperty(objectRatioWater, "value", {
-    get() {
-        return parseInt(ratioWater.value);
-    },
-    set(newValue) {
-        ratioWater.value = newValue;
-    }
+  get() {
+    return parseInt(ratioWater.value);
+  },
+  set(newValue) {
+    ratioWater.value = newValue;
+  },
 });
 Object.defineProperty(objectRecipeCoffee, "value", {
-    get() {
-        return parseInt(recipeCoffee.value);
-    },
-    set(newValue) {
-        recipeCoffee.value = newValue;
-    }
+  get() {
+    return parseInt(recipeCoffee.value);
+  },
+  set(newValue) {
+    recipeCoffee.value = newValue;
+  },
 });
 Object.defineProperty(objectRecipeWater, "value", {
-    get() {
-        return parseInt(recipeWater.value);
-    },
-    set(newValue) {
-        recipeWater.value = newValue;
-    }
+  get() {
+    return parseInt(recipeWater.value);
+  },
+  set(newValue) {
+    recipeWater.value = newValue;
+  },
 });
 
 //set initial values
@@ -49,55 +49,45 @@ objectRatioCoffee.value = 3;
 objectRatioWater.value = 50;
 objectRecipeCoffee.value = 15;
 
-
-
-let getResultWater = () =>{
-    let result;
-    result = (objectRatioWater.value/objectRatioCoffee.value)*objectRecipeCoffee.value;
-    result = objectRecipeWater.value = Math.round(result);
-    return result;
-}
+let getResultWater = () => {
+  let result;
+  result =
+    (objectRatioWater.value / objectRatioCoffee.value) *
+    objectRecipeCoffee.value;
+  result = objectRecipeWater.value = Math.round(result);
+  return result;
+};
 
 // set inital value of objectRecipeWater from default initial values
 getResultWater();
 
-let getResultCoffee = () =>{
-    let result;
-    result = objectRecipeWater.value/(objectRatioWater.value/objectRatioCoffee.value);
-    objectRecipeCoffee.value = result;
-    return result;
-}
+let getResultCoffee = () => {
+  let result;
+  result =
+    objectRecipeWater.value /
+    (objectRatioWater.value / objectRatioCoffee.value);
+  objectRecipeCoffee.value = result;
+  return result;
+};
 
 //handle recipeCoffee input change
-let recipeCoffeeChangeHandler = function() {
-    getResultWater();
-}
+let recipeCoffeeChangeHandler = function () {
+  getResultWater();
+};
 //handle recipeWater input change
-let recipeWaterChangeHandler = function() {
-    getResultCoffee();
-}
+let recipeWaterChangeHandler = function () {
+  getResultCoffee();
+};
 
 //eventlisteners for different browsers?
-if(recipeCoffee.addEventListener)
-    recipeCoffee.addEventListener("change", recipeCoffeeChangeHandler, false);
+if (recipeCoffee.addEventListener)
+  recipeCoffee.addEventListener("change", recipeCoffeeChangeHandler, false);
+else if (recipeCoffee.attachEvent)
+  recipeCoffee.attachEvent("onchange", recipeCoffeeChangeHandler);
+else recipeCoffee.onchange = recipeCoffeeChangeHandler;
 
-else if(recipeCoffee.attachEvent)
-    recipeCoffee.attachEvent("onchange", recipeCoffeeChangeHandler);
-
-else
-    recipeCoffee.onchange = recipeCoffeeChangeHandler;
-
-
-if(recipeWater.addEventListener)
-    recipeWater.addEventListener("change", recipeWaterChangeHandler, false);
-
-else if(recipeWater.attachEvent)
-    recipeWater.attachEvent("onchange", recipeWaterChangeHandler);
-
-else
-    recipeWater.onchange = recipeWaterChangeHandler;
-
-
-
-
-
+if (recipeWater.addEventListener)
+  recipeWater.addEventListener("change", recipeWaterChangeHandler, false);
+else if (recipeWater.attachEvent)
+  recipeWater.attachEvent("onchange", recipeWaterChangeHandler);
+else recipeWater.onchange = recipeWaterChangeHandler;
