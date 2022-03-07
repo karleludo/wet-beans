74dc2e61053903dc8457da7c49e544176f807483
karl_eludo <k_eludo@icloud.com>
2022-03-08 04:12:58 +0800

    Implement two-way databinding

--- src/app/calculator/special-input/special-input.component.ts
+++ src/app/calculator/special-input/special-input.component.ts
@@ -1,19 +1,34 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-special-input',
   templateUrl: './special-input.component.html',
-  styleUrls: ['./special-input.component.css']
+  styleUrls: ['./special-input.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: SpecialInputComponent,
+      multi: true,
+    }
+  ]
 })
-export class SpecialInputComponent implements OnInit {
-  @Input() labelName: string;
+export class SpecialInputComponent implements OnInit, ControlValueAccessor {
+  @Input() labelName!: string;
+  value!: number;
   
+  onChange!: (value: number) => void;
 
-  constructor() { 
-    this.labelName = "Label Text";
+  constructor() {}
+
+  writeValue(obj: any): void {
+    this.value = obj;
   }
-
-  ngOnInit(): void {
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
   }
+  registerOnTouched(fn: any): void {}
 
+  ngOnInit(): void {}
 }
+
