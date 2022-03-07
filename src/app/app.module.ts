acb74f7c265671f1a891093e1b11d7dd25d2fd4a
karl_eludo <k_eludo@icloud.com>
2022-03-08 04:08:27 +0800

    module imports, for reactive forms, formsModule etc.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -19,7 +21,9 @@ import { SpecialInputComponent } from './calculator/special-input/special-input.
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
