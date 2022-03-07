4e977240a51ba2b8051fef49d7612d847831a079
karl_eludo <k_eludo@icloud.com>
2022-03-07 08:30:01 +0800

    app module changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
 import { FooterComponent } from './common/footer/footer.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { SpecialInputComponent } from './calculator/special-input/special-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CalculatorComponent,
+    SpecialInputComponent
   ],
   imports: [
     BrowserModule,
